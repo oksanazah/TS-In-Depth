45814f75fd9fcc2b0ce6d5c14b70c4ddc1739af9
Oksana Zaharchenko <oksana.zaharchenko@gmail.com>
2022-11-25 22:01:17 +0200

    homework

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,192 @@
+/* eslint-disable no-redeclare */
+
 showHello('greeting', 'TypeScript');
 
 function showHello(divName: string, name: string) {
     const elt = document.getElementById(divName);
     elt.innerText = `Hello from ${name}`;
 }
+
+enum Category { JavaScript, CSS, HTML, TypeScript, Angular }
+
+// type Book = {
+//     id: number;
+//     title: string;
+//     author: string;
+//     available: boolean;
+//     category: Category;
+// };
+
+interface Book {
+    id: number;
+    title: string;
+    author: string;
+    available: boolean;
+    category: Category;
+    pages?: number;
+}
+
+// interface A {
+//     [prop: string]: string | number;
+// }
+
+function getAllBooks(): readonly Book[] {
+    const books = <const>[
+        { id: 1, title: 'Refactoring JavaScript', category: Category.JavaScript, author: 'Evan Burchard', available: true },
+        { id: 2, title: 'JavaScript Testing', category: Category.JavaScript, author: 'Liang Yuxian Eugene', available: false },
+        { id: 3, title: 'CSS Secrets', category: Category.CSS, author: 'Lea Verou', available: true },
+        { id: 4, title: 'Mastering JavaScript Object-Oriented Programming', category: Category.JavaScript, author: 'Andrea Chiarelli', available: true }
+    ];
+
+    return books;
+}
+
+function logFirstAvailable(books: readonly Book[] = getAllBooks()): void {
+    console.log(`Number of books: ${books.length}`);
+
+    // const title =  books.find(book => book.available === true).title;
+    const title = books.find(({ available }) => available)?.title;
+    console.log(`Fist avaliable book: ${title}`);
+}
+
+function getBookTitlesByCategory(inputCategory: Category = Category.JavaScript): string[] {
+    const books = getAllBooks();
+
+    // books.filter(book => book.category === inputCategory).map(book => book.title);
+    return books.filter(({ category }) => category === inputCategory).map(({ title }) => title);
+}
+
+function logBookTitles(titles: Array<string>): void {
+    titles.forEach(title => console.log(title));
+}
+
+function getBookAuthorByIndex(index: number): [title: string, author: string] {
+    const books = getAllBooks();
+
+    const { title, author } = books[index];
+    return [title, author];
+}
+
+function calcTotalPages(): void {
+    const data = <const>[
+        { lib: 'libName1', books: 1_000_000_000, avgPagesPerBook: 250 },
+        { lib: 'libName2', books: 5_000_000_000, avgPagesPerBook: 300 },
+        { lib: 'libName3', books: 3_000_000_000, avgPagesPerBook: 280 }
+    ];
+
+    const r = data.reduce((acc: bigint, obj) => {
+        return acc + BigInt(obj.books) * BigInt(obj.avgPagesPerBook);
+    }, 0n);
+
+    console.log(r);
+}
+
+function createCustomerID(name: string, id: number): string {
+    return `${id}/${name}`;
+}
+
+function createCustomer(name: string, age?: number, city?: string): void {
+    console.log(`Customer name: ${name}`);
+
+    if (age) {
+        console.log(`Customer age: ${age}`);
+    }
+
+    if (city) {
+        console.log(`Customer city: ${city}`);
+    }
+}
+
+function getBookByID(id: Book['id']): Book | undefined {
+    const books = getAllBooks();
+    return books.find(book => book.id === id);
+}
+
+function checkoutBooks(customer: string, ...bookIDs: number[]): string[] {
+    console.log(`Customer name: ${customer}`);
+
+    return bookIDs.map(id => getBookByID(id)).filter(book => book.available).map(book => book.title);
+}
+
+function getTitles(author: string): string[];
+function getTitles(available: boolean): string[];
+function getTitles(id: number, available: boolean): string[];
+function getTitles(...args: [string | boolean] | [number, boolean]): string[] {
+    const books = getAllBooks();
+
+    if (args.length === 1) {
+        const [arg] = args;
+
+        if (typeof arg === 'string') {
+            return books.filter(book => book.author === arg).map(book => book.title);
+        } else if (typeof arg === 'boolean') {
+            return books.filter(book => book.available === arg).map(book => book.title);
+        }
+    } else if (args.length === 2) {
+        const [id, available] = args;
+
+        if (typeof id === 'number' && typeof available === 'boolean') {
+            return books.filter(book => book.id === id && book.available === available).map(book => book.title);
+        }
+    }
+}
+
+function assertStringValue(data: any): asserts data is string {
+    if (typeof data !== 'string') {
+        throw new Error('value should have been a string');
+    }
+}
+
+function bookTitleTransform(title: any): string {
+    assertStringValue(title);
+    return [...title].reverse().join('');
+}
+
+function printBook(book: Book): void {
+    console.log(`${book.title} by ${book.author}`);
+}
+
+// logBookTitles(getBookTitlesByCategory(Category.CSS));
+// console.log(getAllBooks());
+// logFirstAvailable(getAllBooks());
+// console.log(getBookAuthorByIndex(0));
+// logBookTitles(['Refactoring JavaScript', 'JavaScript Testing']);
+
+// const myID: string = createCustomerID('Ann', 10);
+// console.log(myID);
+
+// let idGenerator: (name: string, id: number) => string;
+// let idGenerator: typeof createCustomerID;
+// idGenerator = (name: string, id: number) => `${id}/${name}`;
+// idGenerator = createCustomerID;
+
+// const a = typeof createCustomerID;
+// console.log(idGenerator('Boris', 20));
+// createCustomer('Anna');
+// createCustomer('Anna', 20);
+// createCustomer('Anna', 20, 'Kyiv');
+
+// console.log(getBookTitlesByCategory());
+// logFirstAvailable();
+
+// console.log(getBookByID(1));
+
+// console.log(checkoutBooks('NoName Customer', ...[1, 3, 4]));
+
+// console.log(getTitles(1, true));
+// console.log(getTitles(false));
+
+// console.log(bookTitleTransform('Learn TypeScript'));
+
+const myBook: Book = {
+    id: 5,
+    title: 'Colors, Backgrounds, and Gradients',
+    author: 'Eric A. Meyer',
+    available: true,
+    category: Category.CSS,
+    // year: 2015,
+    // copies: 3
+    pages: 200
+};
+
+printBook(myBook);
