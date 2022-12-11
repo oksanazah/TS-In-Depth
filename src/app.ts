7ff2cbcec1072f87ac6fa082810cca4c9b453bdf
Oksana Zaharchenko <oksana.zaharchenko@gmail.com>
2022-12-11 20:52:37 +0200

    homework 2

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable no-redeclare */
 
 showHello('greeting', 'TypeScript');
@@ -17,6 +18,8 @@ enum Category { JavaScript, CSS, HTML, TypeScript, Angular }
 //     category: Category;
 // };
 
+type BookProperties = keyof Book; // | 'isbn';
+
 interface Book {
     id: number;
     title: string;
@@ -24,6 +27,27 @@ interface Book {
     available: boolean;
     category: Category;
     pages?: number;
+    // markDamaged?: (reason: string) => void;
+    // markDamaged?(reason: string): void;
+    markDamaged?: DamageLogger;
+}
+
+interface DamageLogger {
+    (reason: string): void;
+}
+
+interface Person {
+    name: string;
+    email: string;
+}
+
+interface Author extends Person {
+    numBookPublished: number;
+}
+
+interface Librarian extends Person {
+    department: string;
+    assistCustomer: (cusName: string, bookTitle: string) => void;
 }
 
 // interface A {
@@ -146,6 +170,93 @@ function printBook(book: Book): void {
     console.log(`${book.title} by ${book.author}`);
 }
 
+function getProperty(book: Book, prop: BookProperties): any {
+    const value = book[prop];
+
+    return typeof value === 'function' ? value.name : value;
+}
+
+abstract class ReferenceItem {
+    // title: string;
+    // year: number;
+
+    // constructor(newTitle: string, newYear: number) {
+    //     console.log('Creating a new ReferenceItem...');
+    //     this.title = newTitle;
+    //     this.year = newYear;
+    // }
+
+    #id: number;
+
+    private _publisher: string;
+
+    get publisher(): string {
+        return this._publisher.toUpperCase();
+    }
+
+    set publisher(newPublisher: string) {
+        this._publisher = newPublisher;
+    }
+
+    static department: string = 'Research Dep';
+
+    constructor(
+        id: number,
+        public title: string,
+        protected year: number
+    ) {
+        console.log('Creating a new ReferenceItem...');
+        this.#id = id;
+    }
+
+    printItem(): void {
+        console.log(`${this.title} was published in ${this.year}`);
+        console.log(ReferenceItem.department);
+        console.log(Object.getPrototypeOf(this).constructor.department);
+    }
+
+    getID(): number {
+        return this.#id;
+    }
+
+    abstract printCitation(): void;
+}
+
+class Encyclopedia extends ReferenceItem {
+    constructor(
+        id: number,
+        title: string,
+        year: number,
+        public edition: number
+    ) {
+        super(id, title, year);
+    }
+
+    override printItem(): void {
+        super.printItem();
+        console.log(`Edition: ${this.edition} ${this.year}`);
+    }
+
+    printCitation(): void {
+        console.log(`${this.title} - ${this.year}`);
+    }
+}
+
+// interface A {
+//     a: number;
+// }
+class UniversityLibrarian implements Librarian /* , A*/ {
+    name: string;
+    email: string;
+    department: string;
+
+    // a: number = 1;
+
+    assistCustomer(cusName: string, bookTitle: string): void {
+        console.log(`${this.name} is assisting ${cusName} with book ${bookTitle}`);
+    }
+}
+
 // logBookTitles(getBookTitlesByCategory(Category.CSS));
 // console.log(getAllBooks());
 // logFirstAvailable(getAllBooks());
@@ -178,15 +289,68 @@ function printBook(book: Book): void {
 
 // console.log(bookTitleTransform('Learn TypeScript'));
 
-const myBook: Book = {
-    id: 5,
-    title: 'Colors, Backgrounds, and Gradients',
-    author: 'Eric A. Meyer',
-    available: true,
-    category: Category.CSS,
-    // year: 2015,
-    // copies: 3
-    pages: 200
-};
+// const myBook: Book = {
+//     id: 5,
+//     title: 'Colors, Backgrounds, and Gradients',
+//     author: 'Eric A. Meyer',
+//     available: true,
+//     category: Category.CSS,
+//     // year: 2015,
+//     // copies: 3
+//     pages: 200,
+//     // markDamaged: (reason: string) => console.log(`Damaged: ${reason}`)
+//     markDamaged(reason: string) {
+//         console.log(`Damaged: ${reason}`);
+//     }
+// };
 
-printBook(myBook);
+// printBook(myBook);
+// myBook.markDamaged('missing back cover');
+
+// const logDamage: DamageLogger = (reason: string) => console.log(`Damaged: ${reason}`);
+// logDamage('missing back cover');
+
+// const favoritAuthor: Author = {
+//     name: 'Anna',
+//     email: 'anna@example.com',
+//     numBookPublished: 2
+// };
+
+// const favoritLibrarian: Librarian = {
+//     name: 'Boris',
+//     email: 'boris@example',
+//     department: 'Classical Literature',
+//     assistCustomer: null
+// };
+
+// const offer: any = {
+//     book: {
+//         title: 'Essential TypeScript'
+//     }
+// };
+
+// console.log(offer.magazine);
+// console.log(offer.magazine?.getTitle());
+// console.log(offer.book.getTitle?.());
+// console.log(offer.book.author?.[0]);
+
+// console.log(getProperty(myBook, 'title'));
+// console.log(getProperty(myBook, 'markDamaged'));
+// console.log(getProperty(myBook, 'isbn'));
+
+// const ref = new ReferenceItem(1, 'Learn TypeScript', 2022);
+// console.log(ref);
+// ref.printItem();
+// ref.publisher = 'abc group';
+// console.log(ref.publisher);
+// console.log(ref.getID());
+
+// const refBook: Encyclopedia = new Encyclopedia(1, 'Learn TypeScript', 2022, 2);
+// refBook.printItem();
+// console.log(refBook);
+// refBook.printCitation();
+
+const favoritLibrarian: Librarian /* & A*/ = new UniversityLibrarian();
+favoritLibrarian.name = 'Anna';
+favoritLibrarian.assistCustomer('Boris', 'Learn TypeScript');
+// favoritLibrarian.a = 2;
